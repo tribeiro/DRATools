c563f3eb0f07e4dacbc50e46bac13e310882d792
Tiago Ribeiro de Souza <tiago.astro@gmail.com>
2015-04-01 02:29:59 -0300

    Separated the templates in different stochastic variables. The way it was, they where updated in block. I think this causes a convengence problem since you are updating both spectra at the same time.

--- specfit/comp_run.py
+++ specfit/comp_run.py
@@ -1,1 +1,53 @@
+#! /usr/bin/env python
+
 __author__ = 'tiago'
+
+import sys,os
+import numpy as np
+import pylab as py
+
+################################################################################
+
+def main(argv):
+
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    opt,args = parser.parse_args(argv)
+
+    fig1 = py.figure(1)
+    ax1 = fig1.add_subplot(211)
+    ax2 = fig1.add_subplot(212)
+
+    fig2 = py.figure(2)
+    ax3 = fig2.add_subplot(311)
+    ax4 = fig2.add_subplot(312)
+    ax5 = fig2.add_subplot(313)
+
+    fig3 = py.figure(3)
+    ax6 = fig3.add_subplot(111)
+
+    for f in args[1:]:
+        print '->',f
+        rr = np.loadtxt(f,unpack=True,usecols=(2,3,6,7,8))
+        if len(rr) > 4:
+            ax1.plot(rr[0])
+            ax2.plot(rr[1])
+            ax6.hist2d(rr[0],rr[1],bins=[np.arange(0,np.max(rr[0])),np.arange(0,np.max(rr[1]))])
+            ax3.plot(rr[2])
+            ax4.plot(rr[3])
+            ax5.plot(rr[4])
+
+    print 'Done'
+    py.show()
+
+    return 0
+
+################################################################################
+
+if __name__ == '__main__':
+
+    main(sys.argv)
+
+################################################################################
--- specfit/specfitMC.py
+++ specfit/specfitMC.py
@@ -19,129 +19,134 @@ import logging
 ################################################################################
 
 def ModelFactory(ncomp,ofile,templist,temptype=1):
-	'''
+    '''
 A factory of model to do the spectroscopic fit with PyMC.
 
 Input:
-		ncomp		- The number of spectroscopic components to fit.
-		ofile		- The observed spectra.
-		templist	- The template list.
+        ncomp		- The number of spectroscopic components to fit.
+        ofile		- The observed spectra.
+        templist	- The template list.
         temptype    - Type of template spectra. (Chooses read algorithm
                       1 - Pickle
                       2 - SDSSFits
                       3 - Coelho 2014 model spectra
 C - Tiago Ribeiro
-	'''
+    '''
 
-	spMod = spf.SpecFit(ncomp)
+    spMod = spf.SpecFit(ncomp)
 
-	# Load observed spectra as Pickle file
-	
-	if ofile.rfind('.fits') > 0:
-		spMod.loadSDSSFits(ofile,True)
-	elif ofile.rfind('.npy') > 0:
-		spMod.loadPickle(ofile)
-	else:
-		raise IOError('Cannot read %s data type. Only fits and numpy (pickle) \
+    # Load observed spectra as Pickle file
+
+    if ofile.rfind('.fits') > 0:
+        spMod.loadSDSSFits(ofile,True)
+    elif ofile.rfind('.npy') > 0:
+        spMod.loadPickle(ofile)
+    else:
+        raise IOError('Cannot read %s data type. Only fits and numpy (pickle) \
                       files are suported.'%(ofile))
-	
-	# Load template spectra, for each component as Picke files
-	
-	for i in range(ncomp):
-		fp = open(templist[i],'r')
-		line1 = StringIO(fp.readline())
-		line1 = np.loadtxt(line1,dtype='S')
-		spMod.grid_ndim[i] = len(line1)-1 # Set grid dimension for 1st component
 
-		logging.debug('Component %s[%i] with %i dimentions.'%(templist[i],i,spMod.grid_ndim[i]))
+    # Load template spectra, for each component as Picke files
 
-		if temptype == 1:
-			spMod.loadPickleTemplate(i,templist[i])
-		elif temptype == 2:
-			spMod.loadSDSSFitsTemplate(i,templist[i])
-		elif temptype == 3:
-			spMod.loadCoelhoTemplate(i,templist[i])
+    for i in range(ncomp):
+        fp = open(templist[i],'r')
+        line1 = StringIO(fp.readline())
+        line1 = np.loadtxt(line1,dtype='S')
+        spMod.grid_ndim[i] = len(line1)-1 # Set grid dimension for 1st component
 
-	# Pre-process template files
+        logging.debug('Component %s[%i] with %i dimentions.'%(templist[i],i,spMod.grid_ndim[i]))
 
-	spMod.normTemplate(0,5500.,5520.)
-	spMod.normTemplate(1,5500.,5520.)
+        if temptype == 1:
+            spMod.loadPickleTemplate(i,templist[i])
+        elif temptype == 2:
+            spMod.loadSDSSFitsTemplate(i,templist[i])
+        elif temptype == 3:
+            spMod.loadCoelhoTemplate(i,templist[i])
 
-	spMod.preprocTemplate()
+    # Pre-process template files
 
-	# Prepare PyMC stochastic variables
+    spMod.normTemplate(0,5500.,5520.)
+    spMod.normTemplate(1,5500.,5520.)
 
-	maxlevel,minlevel = spMod.suitableScale()
-	level = maxlevel
-	maxlevel *= 2.0
-	minlevel /= 2.0
+    spMod.preprocTemplate()
 
-	min = np.zeros(ncomp)+minlevel
-	val = np.zeros(ncomp)+level
-	max = np.zeros(ncomp)+maxlevel
+    # Prepare PyMC stochastic variables
 
-	scales = pymc.Uniform('scale', min, max, value=val,size=ncomp)
-	min,val,max = np.zeros(ncomp)-600.,np.zeros(ncomp),np.zeros(ncomp)+600.
-	velocity = pymc.Uniform('velocity', min, max , val ,size=ncomp)
+    maxlevel,minlevel = spMod.suitableScale()
+    level = maxlevel
+    maxlevel *= 2.0
+    minlevel /= 2.0
 
-	gridmin = np.zeros(np.sum(spMod.grid_ndim),dtype=int)
-	gridmax = np.zeros(np.sum(spMod.grid_ndim),dtype=int)
-	
-	for i in range(ncomp):
-		logging.debug('gridmax: %i %i %i %i'%(i*2,i*2+1,
-											  len(spMod.Grid[i])-1,
-											  len(spMod.Grid[i][0])-1))
-		for j in range(spMod.grid_ndim[i]):
-			gridmax[i*2+j] = spMod.Grid[i].shape[j]-1
-		#gridmax[i*2+1] = len(spMod.Grid[i][0])-1
+    min = np.zeros(ncomp)+minlevel
+    val = np.zeros(ncomp)+level
+    max = np.zeros(ncomp)+maxlevel
 
-	#max = np.array([len(spMod.template[i])-1 for i in range(ncomp)],dtype=int)
-	val = gridmax/2
+    scales = pymc.Uniform('scale', min, max, value=val,size=ncomp)
+    min,val,max = np.zeros(ncomp)-600.,np.zeros(ncomp),np.zeros(ncomp)+600.
+    velocity = pymc.Uniform('velocity', min, max , val ,size=ncomp)
 
-	template = pymc.DiscreteUniform('template', lower=gridmin, upper=gridmax,
-                                    value=val,
-									size=np.sum(spMod.grid_ndim))
-	# Prepare PyMC deterministic variables
-	
-	@pymc.deterministic
-	def spModel(scales=scales,velocity=velocity,template=template):
-		#logging.debug('spModel: len(spMod.Grid) = %i'%(len(spMod.Grid)))
-		#logging.debug('spModel: Ncomp   Scale   Vel temp1 temp2 lenGrid[0] lenGrid[1]')
-		strlog = "spModel: "
+    gridmin = np.zeros(np.sum(spMod.grid_ndim),dtype=int)
+    gridmax = np.zeros(np.sum(spMod.grid_ndim),dtype=int)
 
-		for idx in range(len(scales)):
-		
-			strlog += '%5i %6.4f %5.1f '%(idx,scales[idx],
-										  velocity[idx])
-			
-			spMod.scale[idx] = scales[idx]
-			spMod.vel[idx] = velocity[idx]
-			index = np.zeros(spMod.grid_ndim[idx],dtype=int)
-			for iidx in range(spMod.grid_ndim[idx]):
-				strlog += '%5i '%template[idx*2+iidx]
-				index[iidx] = template[idx*2+iidx]
-			spMod.ntemp[idx] = spMod.Grid[idx].item(*index)
-			
-		logging.debug(strlog)
-		#	strlog = "spModel: "
-			
-		modspec = spMod.modelSpec()
-		return modspec.flux
+    for i in range(ncomp):
+        logging.debug('gridmax: %i %i %i %i'%(i*2,i*2+1,
+                                              len(spMod.Grid[i])-1,
+                                              len(spMod.Grid[i][0])-1))
+        for j in range(spMod.grid_ndim[i]):
+            gridmax[i*2+j] = spMod.Grid[i].shape[j]-1
+        #gridmax[i*2+1] = len(spMod.Grid[i][0])-1
 
-	mflux = np.mean(spMod.ospec.flux)
-	#sig = pymc.Uniform('sig',
-	#				   np.median(spMod.ospec.err),
-	#				   np.median(spMod.ospec.flux),
-	#				   value=np.median(spMod.ospec.flux)/2.)
-	sig = np.zeros_like(spMod.ospec.err)+np.median(spMod.ospec.flux)/2.
-	#mask = spMod.ospec.x < 5000.
-	#err[mask] = spMod.ospec.err[mask]/10.
-	y = pymc.Normal('y', mu=spModel,
-					tau=1./sig**2.,
-					value=spMod.ospec.flux,
+    #max = np.array([len(spMod.template[i])-1 for i in range(ncomp)],dtype=int)
+    val = gridmax/2
+
+    template = [pymc.DiscreteUniform('template_%02i'%(i+1),
+                                     lower=gridmin[i],
+                                     upper=gridmax[i],
+                                     value=val[i]) for i in range(np.sum(spMod.grid_ndim))]
+
+    #template = pymc.DiscreteUniform('template', lower=gridmin, upper=gridmax,
+    #                                value=val,
+    #								size=np.sum(spMod.grid_ndim))
+    # Prepare PyMC deterministic variables
+
+    @pymc.deterministic
+    def spModel(scales=scales,velocity=velocity,template=template):
+        #logging.debug('spModel: len(spMod.Grid) = %i'%(len(spMod.Grid)))
+        #logging.debug('spModel: Ncomp   Scale   Vel temp1 temp2 lenGrid[0] lenGrid[1]')
+        strlog = "spModel: "
+
+        for idx in range(len(scales)):
+
+            strlog += '%5i %6.4f %5.1f '%(idx,scales[idx],
+                                          velocity[idx])
+
+            spMod.scale[idx] = scales[idx]
+            spMod.vel[idx] = velocity[idx]
+            index = np.zeros(spMod.grid_ndim[idx],dtype=int)
+            for iidx in range(spMod.grid_ndim[idx]):
+                strlog += '%5i '%template[idx*2+iidx]
+                index[iidx] = template[idx*2+iidx]
+            spMod.ntemp[idx] = spMod.Grid[idx].item(*index)
+
+        logging.debug(strlog)
+        #	strlog = "spModel: "
+
+        modspec = spMod.modelSpec()
+        return modspec.flux
+
+    mflux = np.mean(spMod.ospec.flux)
+    sig = pymc.Uniform('sig',
+    				   np.median(spMod.ospec.err),
+    				   np.median(spMod.ospec.flux),
+    				   value=np.median(spMod.ospec.flux)/2.)
+    #sig = np.zeros_like(spMod.ospec.err)+np.median(spMod.ospec.flux)/2.
+    #mask = spMod.ospec.x < 5000.
+    #err[mask] = spMod.ospec.err[mask]/10.
+    y = pymc.Normal('y', mu=spModel,
+                    tau=1./sig**2.,
+                    value=spMod.ospec.flux,
                     observed=True)
 
-	return locals()
+    return locals()
 
 ################################################################################
 
@@ -152,12 +157,12 @@ def main(argv):
                         level=logging.DEBUG)
 
     from optparse import OptionParser
-	
+
     parser = OptionParser()
-    
+
     parser.add_option('-f','--filename',
-                    help = 'Input spectrum to fit.'
-                    ,type='string')
+                      help = 'Input spectrum to fit.'
+                      ,type='string')
     parser.add_option('-o','--output',
                       help = 'Output root name.'
                       ,type='string')
@@ -169,29 +174,29 @@ def main(argv):
 number of components to fit is larger than 1.'''
                       ,type='string')
     parser.add_option('--n-comp',
-					help = '''The number of components to fit. By default only 
+                      help = '''The number of components to fit. By default only
 one is used.''',type='int',default=1)
     parser.add_option('--niter',
-					  help = "Number of iterations (default=10000).",
-					  type='int',default=10000)
+                      help = "Number of iterations (default=10000).",
+                      type='int',default=10000)
     parser.add_option('--burn',
-					  help = "Number of burn iterations (default=2500).",
-					  type='int',default=2500)
+                      help = "Number of burn iterations (default=2500).",
+                      type='int',default=2500)
     parser.add_option('--thin',
-					  help = "Number of sub-iterations for each iteration (default=3).",
-					  type='int',default=3)
+                      help = "Number of sub-iterations for each iteration (default=3).",
+                      type='int',default=3)
     parser.add_option('--tune_interval',
-					  help = "Number of iterations for tuning (default=500).",
-					  type='int',default=500)
+                      help = "Number of iterations for tuning (default=500).",
+                      type='int',default=500)
     parser.add_option('--tune_throughout',
                       help = 'Run in verbose mode (default = False).',action='store_true',
-					  default=False)
+                      default=False)
     parser.add_option('--templatetype',
-					help = '''Type of template file. Choices are: 1 - Pickle,
+                      help = '''Type of template file. Choices are: 1 - Pickle,
 2 - SDSSFits, 3 - Coelho 2014 model spectra''',type='int',default=1)
     parser.add_option('--no-overwrite',
                       help = 'Check if output file exists before writing to it. Save to a different file if it exists.',
-					  action='store_true',
+                      action='store_true',
                       default=False)
     parser.add_option('-v','--verbose',
                       help = 'Run in verbose mode.',action='store_true',
@@ -205,7 +210,7 @@ one is used.''',type='int',default=1)
     opt,args = parser.parse_args(argv)
 
     threadId = os.getpid()
-	
+
     dfile = opt.filename
     outfile = opt.output
     outroot = opt.output[:opt.output.rfind('.')]
@@ -222,10 +227,10 @@ one is used.''',type='int',default=1)
             dbname = outroot + '_%i.%04i.pickle'%(threadId,index)
             index+=1
             logging.debug('%s'%dbname)
-	fp = open(dbname,'w')
-	fp.close()
-	
-	logging.info('dbname: %s'%dbname)
+        fp = open(dbname,'w')
+        fp.close()
+
+        logging.info('dbname: %s'%dbname)
     if opt.no_overwrite and os.path.exists(spname):
         logging.debug('File %s exists (running on "no overwrite" mode).'%spname)
         index = 0
@@ -234,10 +239,10 @@ one is used.''',type='int',default=1)
             spname = outroot + '_%i.spres.%04i.npy'%(threadId,index)
             index+=1
             logging.debug('%s'%spname)
-	fp = open(spname,'w')
-	fp.close()
+        fp = open(spname,'w')
+        fp.close()
 
-	logging.info('spname: %s'%spname)
+        logging.info('spname: %s'%spname)
 
     if opt.no_overwrite and os.path.exists(outfile):
         logging.debug('File %s exists (running on "no overwrite" mode).'%outfile)
@@ -249,8 +254,8 @@ one is used.''',type='int',default=1)
             index+=1
             logging.debug('%s'%outfile)
 
-	fp = open(outfile,'w')
-	fp.close()
+        fp = open(outfile,'w')
+        fp.close()
 
     logging.info('outfile: %s'%outfile)
 
@@ -265,25 +270,28 @@ one is used.''',type='int',default=1)
             index+=1
             logging.debug('%s'%csvname)
 
-	fp = open(csvname,'w')
-	fp.close()
+        fp = open(csvname,'w')
+        fp.close()
 
-	logging.info('csvname: %s'%csvname)
+        logging.info('csvname: %s'%csvname)
 
     logging.info('Preparing model...')
-	
+
     if opt.n_comp > 1:
-		tlist = np.loadtxt(opt.template_list,dtype='S')
+        tlist = np.loadtxt(opt.template_list,dtype='S')
     elif not opt.grid:
-		tlist = np.loadtxt(opt.template_list,dtype='S',ndim=1)
+        tlist = np.loadtxt(opt.template_list,dtype='S',ndim=1)
 
     M = pymc.MCMC(	ModelFactory( opt.n_comp, dfile,
-                                     tlist, opt.templatetype) ,
-                    db = 'ram')#,
-					#dbname=dbname)
+                                    tlist, opt.templatetype) ,
+                      db = 'ram')#,
+    #dbname=dbname)
 
-    M.use_step_method(pymc.DiscreteMetropolis, M.template,
-					  proposal_distribution='Normal')
+    #M.use_step_method(pymc.DiscreteMetropolis, M.template,
+    #                  proposal_sd=M.gridmax/5.,
+    #                  proposal_distribution='Normal')
+
+
     #M.use_step_method(pymc.AdaptiveMetropolis,)
     logging.info('Model done...')
 
@@ -311,7 +319,7 @@ one is used.''',type='int',default=1)
     py.show()
 
     '''
-	
+
     #print M.spMod.templateScale
     #return 0
 
@@ -322,16 +330,22 @@ one is used.''',type='int',default=1)
     if NBURN > NITER:
         NITER+=NBURN
     M.sample(iter=NITER,burn=NBURN,thin=opt.thin,
-			tune_interval=opt.tune_interval,tune_throughout=opt.tune_throughout,
-			verbose=-1)
-#,tune_interval=1000,tune_throughout=True,verbose=0)
+             tune_interval=opt.tune_interval,tune_throughout=opt.tune_throughout,
+             verbose=-1)
+    #,tune_interval=1000,tune_throughout=True,verbose=0)
 
     logging.info('Sampler done. Saving results...')
 
     M.db.close()
 
-    M.write_csv(csvname,variables=['scale','velocity','template'])
-	
+    #ovars = ['scale','velocity']
+    #for i in range(np.sum(M.spMod.grid_ndim)):
+    #    ovars.append('template_%02i'%(i+1))
+
+    #print ovars
+
+    M.write_csv(csvname) #,variables=ovars)
+
     #grid = np.array(M.trace('template')[:]).reshape(2,-1)
     dtype = [('scale1', '<f8'), ('vel1', '<f8'),
              ('scale2', '<f8'), ('vel2', '<f8'),
@@ -342,7 +356,7 @@ one is used.''',type='int',default=1)
             dtype.append(('temp%i_%i'%(i,j),'<i4'))
 
     oarray = np.zeros(	len(M.trace('scale')[:]),
-                        dtype=dtype)
+                          dtype=dtype)
 
 
     oarray['scale1'] = np.array(	[i[0] for i in M.trace('scale')[:]] )
@@ -353,20 +367,20 @@ one is used.''',type='int',default=1)
     for i in range(M.ncomp):
         index = np.zeros(M.spMod.grid_ndim[i])
         for j in range(M.spMod.grid_ndim[i]):
-            oarray['temp%i_%i'%(i,j)] = np.array(	[ii[i*2+j] for ii in M.trace('template')[:]] )
+            oarray['temp%i_%i'%(i,j)] = np.array(	[ii for ii in M.trace('template_%02i'%(i*2+j+1))[:]] )
             index[j] = np.int(scipy.stats.mode(oarray['temp%i_%i'%(i,j)])[0][0])
         M.spMod.ntemp[i] = M.spMod.Grid[i].item(*index)
 
     #oarray['temp2_3'] = np.array(	[i[4] for i in M.trace('template')[:]] )
     #oarray['sig'] = np.array(	[i for i in M.trace('sig')[:]] )
-	
+
     for idx in range(2):
-		for icomp in range(len(oarray['temp1_1'])):
-			index = np.zeros(M.spMod.grid_ndim[idx],dtype=int)
-			for iidx in range(M.spMod.grid_ndim[idx]):
-				index[iidx] = oarray['temp%i_%i'%(idx,iidx)][icomp]
-			ntemp = M.spMod.Grid[idx].item(*index)
-			oarray['tempscale%i'%(idx+1)][icomp] = M.spMod.templateScale[idx][ntemp]
+        for icomp in range(len(oarray['temp1_1'])):
+            index = np.zeros(M.spMod.grid_ndim[idx],dtype=int)
+            for iidx in range(M.spMod.grid_ndim[idx]):
+                index[iidx] = oarray['temp%i_%i'%(idx,iidx)][icomp]
+            ntemp = M.spMod.Grid[idx].item(*index)
+            oarray['tempscale%i'%(idx+1)][icomp] = M.spMod.templateScale[idx][ntemp]
 
 
     np.save(outfile,oarray)
@@ -388,17 +402,17 @@ one is used.''',type='int',default=1)
     mspec2 = M.spMod.modelSpec()
 
     sp_array = np.zeros(	len(mspec.x),
-				  dtype=[('wave', '<f8'), ('data', '<f8'), ('model', '<f8'),
-						 ('model1', '<f8') , ('model2', '<f8')])
+                            dtype=[('wave', '<f8'), ('data', '<f8'), ('model', '<f8'),
+                                   ('model1', '<f8') , ('model2', '<f8')])
 
     sp_array['wave'] = mspec.x
     sp_array['data'] = M.spMod.ospec.flux
     sp_array['model'] = mspec.flux
     sp_array['model1'] = mspec1.flux
     sp_array['model2'] = mspec2.flux
-	
+
     np.save(spname,sp_array)
-	
+
     py.subplot(241)
     hh,edges = np.histogram(oarray['scale1'],range=[M.minlevel,M.maxlevel*1.2])
     width = np.mean(edges[1:]-edges[:-1])/2.
@@ -411,7 +425,7 @@ one is used.''',type='int',default=1)
     py.subplot(242)
     py.hist(oarray['vel1'])
     py.hist(oarray['vel2'])
-	
+
     py.subplot(243)
 
     hh,edges = np.histogram(oarray['temp0_0'],bins=np.arange(-0.5,M.gridmax[0]))
@@ -422,7 +436,7 @@ one is used.''',type='int',default=1)
     py.bar(edges[:-1]+0.1,hh+1e-3)
 
     py.subplot(212)
-	
+
     py.plot(M.spMod.ospec.x,M.spMod.ospec.flux,'0.8')
     py.plot(mspec.x,mspec.flux,'k')
 
@@ -440,7 +454,7 @@ one is used.''',type='int',default=1)
 
         logging.info('Saving figure to %s'%(opt.output+'.png'))
 
-        py.savefig(os.path.join(pltdir,plotname+'.png'))
+        py.savefig(os.path.join(pltdir,os.path.basename(plotname)+'.png'))
 
         pymc.Matplot.plot(M,
                           path = pltdir)
